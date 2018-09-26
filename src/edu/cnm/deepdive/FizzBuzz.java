3ec3ce7b608b91145b284e54e2c4424bdc14b2bb
Matthew Morgan <mmorgan55@cnm.edu>
2018-09-26 13:06:06 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -0,0 +1,24 @@
+package edu.cnm.deepdive;
+
+public class FizzBuzz {
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 110; i++) {
+      String value = "";
+      if (i % 3 == 0) {
+        value += "Fizz";
+      }
+      if (i % 5 == 0) {
+        value += "Buzz";
+      }
+      if (i % 7 == 0) {
+        value += "Woof";
+      }
+      if (value.isEmpty()) {
+        value += i;
+      }
+      System.out.println(value);
+    }
+
+  }
+}
